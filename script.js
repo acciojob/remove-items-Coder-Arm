f93c323ec7f4d38441f2ce286e1771a767e3ef18
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-06-17 20:17:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+function callme(){
+		const selectElement = document.getElementById("colorSelect");
+	//total options
+     let selectOptions = selectElement.options;
+	//selected option
+     let selected = selectOptions.selectedIndex;
+	//remove selected option
+	selectElement.remove(selected);
+}
+btn.addEventListener("click",callme);
